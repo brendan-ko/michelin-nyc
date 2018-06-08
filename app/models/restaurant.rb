35aa4b71b0a4384cfa5769f9383af3e8850a537f
Brendan Ko <brendan.ko@plated.com>
2018-06-08 13:58:32 -0400

    Add null validations to restaurant model

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,2 +1,16 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string           not null
+#  address     :text             not null
+#  description :text             not null
+#  stars       :hstore
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 class Restaurant < ApplicationRecord
+  validates :name, :address, :description, :stars, presence: true
 end
--- test/models/restaurant_test.rb
+++ test/models/restaurant_test.rb
@@ -1,3 +1,16 @@
+# == Schema Information
+#
+# Table name: restaurants
+#
+#  id          :bigint(8)        not null, primary key
+#  name        :string           not null
+#  address     :text             not null
+#  description :text             not null
+#  stars       :hstore
+#  created_at  :datetime         not null
+#  updated_at  :datetime         not null
+#
+
 require 'test_helper'
 
 class RestaurantTest < ActiveSupport::TestCase
