985118e50e97ff6303fc2887f6f8f06040663c9a
Brendan Ko <brendan.ko@plated.com>
2018-06-08 13:51:26 -0400

    Add restaurant migration

--- db/migrate/20180608144013_create_restaurants.rb
+++ db/migrate/20180608144013_create_restaurants.rb
@@ -1,8 +1,12 @@
 class CreateRestaurants < ActiveRecord::Migration[5.2]
   def change
+    enable_extension 'hstore' unless extension_enabled?('hstore')
     create_table :restaurants do |t|
-
+      t.string :name, null: false
+      t.text :address, null: false
+      t.text :description, null: false
+      t.hstore :stars
       t.timestamps
     end
-  end
+    add_index :restaurants, :stars, using: :gist  end
 end
