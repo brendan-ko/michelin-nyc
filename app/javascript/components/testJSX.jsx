aa85a76a1d79f6c859ab26ab43cc49670f545dce
Brendan Ko <brendan.ko@plated.com>
2018-06-11 10:36:30 -0400

    Create working test JSX with react-rails

---
+++ app/javascript/components/testJSX.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const testJSX = (props) => {
+  return (
+    <div>
+      <h1>SUP BRO</h1>
+      <h2>the text prop should be here: {props.text}</h2>
+    </div>
+  )
+}
+
+export default testJSX;
